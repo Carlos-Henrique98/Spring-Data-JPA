85089972175d167377b5da33ec46555cc980dc7a
Carlos Henrique <ichfs9108@gmail.com>
2022-06-10 15:26:14 -0300

    atualizando o registro

--- spring-data/src/main/java/br/com/alura/springdata/service/CrudCargoService.java
+++ spring-data/src/main/java/br/com/alura/springdata/service/CrudCargoService.java
@@ -10,6 +10,7 @@ import br.com.alura.springdata.repository.CargoRepository;
 @Service
 public class CrudCargoService {
 
+	private Boolean system = true;
 	private final CargoRepository cargoRepository;
 
 	public CrudCargoService(CargoRepository cargoRepository) {
@@ -17,6 +18,28 @@ public class CrudCargoService {
 	}
 
 	public void inicial(Scanner scanner) {
+		while (system) {
+			System.out.println("Qual acao de cargo deseja executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Salvar");
+			System.out.println("2 - Atualizar");
+
+			int action = scanner.nextInt();
+
+			switch (action) {
+			case 1: {
+				salvar(scanner);
+				break;
+			}
+			case 2: {
+				atualizar(scanner);
+				break;
+			}
+			default:
+				system = false;
+				break;
+			}
+		}
 		salvar(scanner);
 	}
 
@@ -27,5 +50,17 @@ public class CrudCargoService {
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Salvo");
+	}
+
+	private void atualizar(Scanner scanner) {
+		System.out.println("Id");
+		int id = scanner.nextInt();
+		System.out.println("Descrição do Cargo");
+		String descrição = scanner.next();
+
+		Cargo cargo = new Cargo();
+		cargo.setId(id);
+		cargo.setDescricao(descrição);
+		cargoRepository.save(cargo);
 	}
 }
